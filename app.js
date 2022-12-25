b3c004e04b512d78a18ed3b8d2af0e549596a1ff
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-12-25 12:31:42 +0200

    Урок 1-2

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
 
+//! Middleware - Парсер JSON
+app.use(express.json());
 
+//!!!!! ===== ГЛАВНЫЙ ОБРАБОТЧИК ВСЕХ ЗАПРОСОВ  ===== 
+app.use("/", (req, res, next) => {
+    console.log("Hello, Middleware-1");
+    next()
+});
 
+app.use("/", (req, res, next) => {
+    console.log("Hello, Middleware-2");
+    next()
+});
 
 
 module.exports = {
